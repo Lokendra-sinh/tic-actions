22e6ef9a65dc27849bc18f3410a438d0650919fa
Lokendra-sinh <anizen242@gmail.com>
2024-03-30 20:32:28 +0530

    row and column titles added for moves

--- generateGameBoard.js
+++ generateGameBoard.js
@@ -1,40 +1,52 @@
+const { createCanvas } = require("canvas");
 
-const { createCanvas } = require('canvas');
+function generateGameBoard() {
+  const containerHeight = 500;
+  const containerWidth = 500;
+  const canvasHeight = containerHeight + 200;
+  const canvasWidth = containerWidth + 200;
+  const boxSize = containerHeight / 3;
+  const row = ["A", "B", "C"];
+  const col = ["1", "2", "3"];
+  const canvas = createCanvas(canvasWidth, canvasHeight);
+  const ctx = canvas.getContext("2d");
 
-function generateGameBoard(){
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  ctx.strokeStyle = "white";
+  ctx.strokeRect(100, 100, containerWidth, containerHeight);
 
-    const containerHeight = 500;
-    const containerWidth = 500;
-    const canvasHeight = containerHeight + 200;
-    const canvasWidth = containerWidth + 200;
-    const boxSize = containerHeight / 3;
-    const canvas = createCanvas(canvasWidth, canvasHeight);
-    const ctx = canvas.getContext('2d');
-    
-    ctx.beginPath();
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    ctx.strokeStyle = 'white';
-    ctx.strokeRect(100, 100, containerWidth, containerHeight);
+  for (let i = 1; i <= 2; i++) {
+      const x = 100 + i * boxSize;
+      ctx.moveTo(x, 100);
+      ctx.lineTo(x, 100 + containerHeight);
+  }
 
+  for (let i = 1; i <= 2; i++) {
+    const y = 100 + i * boxSize;
+    ctx.moveTo(100, y);
+    ctx.lineTo(100 + containerWidth, y);
+  }
 
-    for (let i = 1; i <= 2; i++) {
-        const x = 100 + i * boxSize;
-        ctx.moveTo(x, 100);
-        ctx.lineTo(x, 100 + containerHeight);
-      }
+  // render row titles (A, B, C)
+  for(let i = 0; i < 3; i++){
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "blue";
+    ctx.fillText(row[i], 50, 100 + (i + 1) * boxSize - 50);
+  }
 
-      for (let i = 1; i <= 2; i++) {
-        const y = 100 + i * boxSize;
-        ctx.moveTo(100, y);
-        ctx.lineTo(100 + containerWidth, y);
-      }
+    // render column titles (1, 2, 3)
+    for(let i = 0; i < 3; i++){
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "green";
+        ctx.fillText(col[i], 100 + (i + 1) * boxSize - 50, 50);
+        }
 
-      ctx.stroke();
-      ctx.closePath();
+  ctx.stroke();
+  ctx.closePath();
 
-        return canvas.toBuffer();
-
+  return canvas.toBuffer();
 }
 
 module.exports = generateGameBoard;
